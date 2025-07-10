5f97d6c320f76574050363b8f2d72b9c45708023
Jorge <jromo1506mx@gmail.com>
2025-07-10 14:15:22 -0600

    QA

--- src/app/pages/blog/blog.component.spec.ts
+++ src/app/pages/blog/blog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlogComponent } from './blog.component';
+import { By } from '@angular/platform-browser';
 
 describe('BlogComponent', () => {
   let component: BlogComponent;
@@ -18,4 +19,17 @@ describe('BlogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it("Post debe ser '3'", () => {
+    expect(component.post).toBe("3");
+  });
+
+  it('Debe renderizar 3 app-posts',() => {
+    const post = fixture.debugElement.queryAll(By.css('app-post'));
+    expect(post.length).toBe(3);
+  });
+
+
+
 });
